914568029e1604b5c175e52a5257dda4964b78be
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-12-15 16:22:44 +0100

    show more buttone done

--- src/HompageComponents/LabmembersSection.js
+++ src/HompageComponents/LabmembersSection.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Badge, Col, Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { FaArrowCircleRight } from '@react-icons/all-files/fa/FaArrowCircleRight';
 import { useInView } from "react-intersection-observer";
 import LazyLoad from 'react-lazyload';
+import { HashLink as Link } from 'react-router-hash-link'
 
 
 function LabmembersSection({ scrollPosition }) {
@@ -12,19 +12,22 @@ function LabmembersSection({ scrollPosition }) {
   const { ref: myLab2, inView: myLab2IsVisible } = useInView({ triggerOnce: true })
   const { ref: myLab3, inView: myLab3IsVisible } = useInView({ triggerOnce: true })
   const { ref: myLab4, inView: myLab4IsVisible } = useInView({ triggerOnce: true })
-
-
-
+  const [membersExtra, setmembersExtra] = useState([]);
+  const [show, setShow] = useState(false);
   const [labmemberList, setLabmemberList] = useState([]);
   const fetchLabmembers = async () => {
     var sortOrder = ["Assistant Professor", "Postdoc", "PhD student", "visiting PhD student", "Master student"];
     let res = await fetch("https://sglobe-server.onrender.com/labmembers/fetch-labmembers");
     let data = await res.json();
-    setLabmemberList(data);
+  
     var sorted = data.sort((a, b) => sortOrder.indexOf(a.functionbasic
     ) - sortOrder.indexOf(b.functionbasic
     ));
-    console.log(sorted);
+    let sortedFirst=data.slice(0,7)
+    let sortedRest=data.slice(7)
+    console.log(sortedFirst);
+    setLabmemberList(sortedFirst);
+    setmembersExtra(sortedRest);
   };
   useEffect(() => {
     fetchLabmembers();
@@ -112,6 +115,43 @@ function LabmembersSection({ scrollPosition }) {
             </Col>
           ) : ("")
         })}
+       {membersExtra.map((member, index) => {
+          return member.currentmember === "Yes" && member.functionbasic !== 'Assistant Professor' ? (
+            // Grid system with shifting number of elements
+   show===true?(         <Col
+    xs={8}
+    md={4}
+    lg={3}
+    xxl={2}
+    className={`d-flex justify-content-center align-items-center memberpicturepadding divslide-before ${myLab3IsVisible ? "divslide2" : ""} `}
+    key={index}
+
+  >
+    <Link to={`/labmember/${member.membername.replace(/\s/g, '-').toLowerCase()}`} className="photo-link" >
+      <div className={`members d-flex justify-content-center align-items-center memberpicturepadding `}
+      >
+        <LazyLoad><img
+          alt={member.membername}
+          key={member.membername}
+          src={`${member.image}`}
+          className='member-photo'
+        /></LazyLoad>
+        <div className="member-photo-overlay"></div>
+        <div className="member-text-overlay">
+          <h3 className="montserrat membername h5">
+            {member.membername}
+          </h3>
+          <h5 className="roboto memberfunction h6">
+            {member.functionbasic}
+          </h5>
+        </div>
+      </div>
+    </Link>
+  </Col>):("")
+          ) : ("")
+        })}
+<Col  md={12}>{show ? (<Link to="/#labmembers" className={`pt-5 buttonpadding `}><Badge onClick={() => setShow(false)} className={` badge-icon montserrat `}>Show less</Badge></Link>) : (<Badge onClick={() => setShow(true)} className={` badge-icon montserrat more-projects `}>View all labmembers</Badge>)}</Col>
+
         {/* Alumni Section*/}
         <Link to={"/alumni"} className="pt-5 buttonpadding"><h3 className={`d-flex divslide-before  ${myLab4IsVisible ? "divslide" : ""} `} ref={myLab4}><Badge className="badge-icon montserrat">SEE ALUMNI <FaArrowCircleRight className="arrow-icon" /></Badge></h3></Link>
       </Row>
--- src/HompageComponents/ProjectsSction.js
+++ src/HompageComponents/ProjectsSction.js
@@ -1,63 +1,102 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Badge, Col, Container, Row } from "react-bootstrap";
 import { useInView } from "react-intersection-observer";
 import LazyLoad from 'react-lazyload';
+import { HashLink as Link } from 'react-router-hash-link'
 
 
 function ProjectsSction() {
   const [projects, setProjects] = useState([]);
   const [projectsExtra, setProjectsExtra] = useState([]);
+  const [show, setShow] = useState(false);
   const { ref: myProj, inView: myProjIsVisible } = useInView({ triggerOnce: true })
   const { ref: myProj2, inView: myProj2IsVisible } = useInView({ triggerOnce: true })
+  const { ref: myProj3, inView: myProj3IsVisible } = useInView({ triggerOnce: true })
 
 
   const fetchProjectList = async () => {
     let res = await fetch("https://sglobe-server.onrender.com/projects/fetch-projects");
     let data = await res.json();
-
-    setProjects(data.slice(2));
-    console.log(projects)
+    let firstLine = data.slice(0, 3)
+    let rest = data.slice(3)
+    setProjects(firstLine);
+    setProjectsExtra(rest)
+    console.log(firstLine)
+    console.log(rest)
+    console.log(data)
   };
   useEffect(() => {
     fetchProjectList();
   }, []);
 
 
-  return (
-      <Container fluid className='mt-0 mb-0'>{}
-        <h1 className={` proj-section-title montserrat pb-4 divslide-before ${myProjIsVisible ? "divslide" : ""}`} ref={myProj}>Research projects</h1>
-        <div className="d-flex flex-wrap justify-content-center">
-          <Row ref={myProj2}>
-            {projects.map((project, index) => (
-                  <Col key={index} md={6} lg={4} className={`row-elem divslide-before  ${myProj2IsVisible ? "divslide2" : ""}`}  >
-                  <Link to={`/project/${project.title.replace(/\s/g, '-').toLowerCase()}`} className="project-link">
-                    <LazyLoad className=" projects d-flex justify-content-center" >
-                    
-                      <img
-                        src={project.image}
-                        className="project-photo" alt={project.title}
-                      />
-                      <div className="project-photo-overlay" ></div>
-                      <div className="project-text-overlay">
-                        <h3 className="montserrat projecttitle pe-3">
-                          {project.title}
-                        </h3>
-                      </div>
-                      <div className="project-summary-container" >
-                        <p className="roboto project-summary mb-1">
-                          {project.summary}.</p><p className='roboto project-summary readmore'> Read more ...
-                        </p>
-                      </div>
-                      </LazyLoad>
-                  </Link>
-                </Col>
-            ))}
-          </Row>
-        </div>
-      </Container>
- 
-  );
+  return <Container fluid className='mt-0 mb-0'>
+    <h1 className={` proj-section-title montserrat pb-4 divslide-before ${myProjIsVisible ? "divslide" : ""}`} ref={myProj}>Research projects</h1>
+    <div className="d-flex flex-wrap justify-content-center">
+      <Row ref={myProj2}>
+        {projects.map((project, index) => (
+          <Col key={index} md={6} lg={4} className={`row-elem divslide-before  ${myProj2IsVisible ? "divslide2" : ""}`}  >
+            <Link to={`/project/${project.title.replace(/\s/g, '-').toLowerCase()}`} className="project-link">
+              <LazyLoad className=" projects d-flex justify-content-center" >
+
+                <img
+                  src={project.image}
+                  className="project-photo" alt={project.title}
+                />
+                <div className="project-photo-overlay" ></div>
+                <div className="project-text-overlay">
+                  <h3 className="montserrat projecttitle pe-3">
+                    {project.title}
+                  </h3>
+                </div>
+                <div className="project-summary-container" >
+                  <p className="roboto project-summary mb-1">
+                    {project.summary}.</p><p className='roboto project-summary readmore'> Read more ...
+                  </p>
+                </div>
+              </LazyLoad>
+            </Link>
+          </Col>
+
+        ))}
+
+        {projectsExtra.map((project, index) => {
+         return show === true?
+            (
+
+              <Col key={index} md={6} lg={4} className={`row-elem divslide-before  ${myProj2IsVisible ? "divslide2" : ""}`}  >
+                <Link to={`/project/${project.title.replace(/\s/g, '-').toLowerCase()}`} className="project-link">
+                  <LazyLoad className=" projects d-flex justify-content-center" >
+
+                    <img
+                      src={project.image}
+                      className="project-photo" alt={project.title}
+                    />
+                    <div className="project-photo-overlay" ></div>
+                    <div className="project-text-overlay">
+                      <h3 className="montserrat projecttitle pe-3">
+                        {project.title}
+                      </h3>
+                    </div>
+                    <div className="project-summary-container" >
+                      <p className="roboto project-summary mb-1">
+                        {project.summary}.</p><p className='roboto project-summary readmore'> Read more ...
+                      </p>
+                    </div>
+                  </LazyLoad>
+                </Link>
+              </Col>
+            ):("")
+         
+        })}
+       
+      </Row>
+        <Col ref={myProj3} md={12}>{show ? (<Link to="/#projects" className={`pt-5 buttonpadding ${myProj3IsVisible ? "divslide" : ""}`}><Badge onClick={() => setShow(false)} className={` badge-icon montserrat `}>Show less</Badge></Link>) : (<Badge onClick={() => setShow(true)} className={` badge-icon montserrat more-projects ${myProj3IsVisible ? "divslide" : ""}`}>View all projects</Badge>)}</Col>
+   
+    </div>
+  </Container>
+
+
 }
 
 export default ProjectsSction;
